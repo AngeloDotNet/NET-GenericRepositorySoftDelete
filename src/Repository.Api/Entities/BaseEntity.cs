070dd6024d17727493ac32c03573e6009010d673
Angelo Pirola <angelo9871@gmail.com>
2026-03-04 08:59:26 +0100

    Add BaseEntity and Product with soft-delete support
    Introduced BaseEntity abstract class with Id property for entity inheritance. Added Product entity inheriting from BaseEntity and implementing ISoftDelete, including Name, Price, IsDeleted, and DeletedAt properties for soft-delete functionality. Product references ISoftDelete from the appropriate namespace.

---
+++ src/Repository.Api/Entities/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Repository.Api.Entities;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+}
---
+++ src/Repository.Api/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using Repository.Api.Entities.Interfaces;
+
+namespace Repository.Api.Entities;
+
+public class Product : BaseEntity, ISoftDelete
+{
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+
+    // soft-delete props
+    public bool IsDeleted { get; set; }
+    public DateTime? DeletedAt { get; set; }
+}
