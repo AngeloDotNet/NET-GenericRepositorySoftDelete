2baba130eb1e8769dcb185e9030c78fff0adb295
Angelo Pirola <angelo9871@gmail.com>
2026-03-04 08:59:36 +0100

    Add generic repository and unit of work interfaces
    Introduce IGenericRepository<T> and IUnitOfWork interfaces under Repository.Api.Repositories.Interfaces. These provide a foundation for abstracted, testable data access with async CRUD operations, advanced querying, and support for both soft and hard deletes.

---
+++ src/Repository.Api/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using Repository.Api.Entities;
+
+namespace Repository.Api.Repositories.Interfaces;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    IQueryable<T> Query(); // per query avanzate
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task AddAsync(T entity);
+    void Update(T entity);
+    Task DeleteAsync(T entity); // soft delete
+    Task HardDeleteAsync(T entity); // hard delete
+}
---
+++ src/Repository.Api/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Repository.Api.Entities;
+
+namespace Repository.Api.Repositories.Interfaces;
+
+public interface IUnitOfWork
+{
+    IGenericRepository<T> Repository<T>() where T : BaseEntity;
+    Task<int> SaveChangesAsync();
+}
